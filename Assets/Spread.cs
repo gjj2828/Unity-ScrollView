75e717491dd9745f02a13a4bbba6221972bd895e
gjj2828 <gjj2828@gmail.com>
2020-09-11 08:03:54 +0800

    ScrollView

---
+++ Assets/Spread.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spread : MonoBehaviour
+{
+    public GameObject sub;
+    public RectTransform root;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            sub.SetActive(!sub.activeSelf);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(root);
+        });
+    }
+}
